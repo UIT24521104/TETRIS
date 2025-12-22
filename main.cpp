e9cbf7a76ccab0703d9b1f23c938082fe40c8149
Ngô Trí Hải <24520436@gm.uit.edu.vn>
2025-12-22 10:17:39 +0700

    addition removeline
    Hàm removeline đã chạy đúng

--- main.cpp
+++ main.cpp
@@ -241,6 +241,31 @@ void draw(){
     cout.flush();
 }
 
+void removeLine(){
+    for (int i = H - 2; i >= 1; i--){
+        bool full = true;
+        for (int j = 1; j < W - 1; j++){
+            if (board[i][j] != BLOCK) {
+                full = false;
+                break;
+            }
+        }
+        if (full) {
+            for (int k = i; k > 0; k--){
+                for (int j = 1; j < W - 1; j++){
+                    board[k][j] = board[k - 1][j];
+                }
+            }
+            for (int j = 1; j < W - 1; j++){
+                board[0][j] = ' ';
+            }
+            score += 100;
+            i++;  
+        }
+    }
+}
+
+
 int getRandomX(Block*p) {
     if (!p) return 0;
     int blockMaxCol = p->getMaxCol();
